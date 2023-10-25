8e8cbba7b8070940c64866c3aa6ffe2b9617c898
Sean <seanye86@gmail.com>
2023-10-24 19:51:29 -0600

    src code modificaiton

--- Portfolio-Main/src/components/ProjectPage.jsx
+++ Portfolio-Main/src/components/ProjectPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import '../components/work.css';
 import animefusion from '/src/assets/Images/animefusion.jpg';
-import tastefultwosome from '/src/assets/Images/tastfultwosomes.png';
+import tastefultwosome from '/src/assets/Images/tastfultwosomes.jpeg';
+import weatherdash from '/src/assets/Images/weatherdash.jpg';
+import socialnetwork from '/src/assets/Images/socialnetworkapi.jpg';
+import ecommerce from '/src/assets/Images/ecommerce.jpg';
+import employeetracker from '/src/assets/Images/employeetracking.jpg';
 
 export default function ProjectPage() {
 return (
@@ -20,20 +24,35 @@ return (
     <h2 className="code_types2">CSS/HTML</h2>
   </a>
 </article>
-{/* <article className="Work_Images">
-  <a href="https://github.com/Seanye333/Simple_Camera_Product_Page.git" target="_blank">
-    <h1 className="images_title2">Camera Product</h1>
-    <img className="Mid" src='' alt="A camera page with camera product in the middle and description of product and price to its left" />
+<article className="Work_Images">
+  <a href="https://github.com/Seanye333/Challenge-6-Weather-Dashboard" target="_blank">
+    <h1 className="images_title2">Weather-Dashboard</h1>
+    <img className="Mid" src= {weatherdash} alt="An overview of CSS blog webpage talking about building responsive layout" />
     <h2 className="code_types2">CSS/HTML</h2>
   </a>
 </article>
 <article className="Work_Images">
-  <a href="https://github.com/Seanye333/CSS_Blog_Web.git" target="_blank">
-    <h1 className="images_title2">CSS Blog Web</h1>
-    <img className="Mid" src="./assets/Images/CSS_Blog_Web.JPG" alt="An overview of CSS blog webpage talking about building responsive layout" />
+  <a href="https://github.com/Seanye333/Social-Network-API" target="_blank">
+    <h1 className="images_title2">Social-Network-API</h1>
+    <img className="Mid" src= {socialnetwork} alt="A camera page with camera product in the middle and description of product and price to its left" />
     <h2 className="code_types2">CSS/HTML</h2>
   </a>
-</article> */}
+</article>
+<article className="Work_Images">
+  <a href="https://github.com/Seanye333/E-Commerce-Back-End" target="_blank">
+    <h1 className="images_title2">E-Commerce</h1>
+    <img className="Mid" src= {ecommerce} alt="A camera page with camera product in the middle and description of product and price to its left" />
+    <h2 className="code_types2">CSS/HTML</h2>
+  </a>
+</article>
+<article className="Work_Images">
+  <a href="https://github.com/Seanye333/C12-Employee-Tracker" target="_blank">
+    <h1 className="images_title2">Employee-Tracker</h1>
+    <img className="Mid" src= {employeetracker} alt="A camera page with camera product in the middle and description of product and price to its left" />
+    <h2 className="code_types2">CSS/HTML</h2>
+  </a>
+</article>
+
 </section>
 )
 }
